0fe6543d04ecd121a4e2fcc05bcaa077a4214c28
successfulcode <56683358+successfulcode@users.noreply.github.com>
2021-09-12 19:45:42 +0300

    Added vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
-require('./bootstrap');
+import Vue from "vue";
+import App from "./views/app.vue";
+
+const app = new Vue({
+    el: "#app",
+    components: { App },
+});
---
+++ resources/js/views/app.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>Hey Vue!</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
